1b42e86fc8baf87971a91ffb9011c6b31fad2179
guitopaixao <99556693+guitopaixao@users.noreply.github.com>
2022-03-03 18:41:02 -0300

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,168 @@
+window.sr = ScrollReveal();
+
+    //TEXTO-HOME  
+	sr.reveal('.home__data', {
+		duration: 3000,
+		origin: 'bottom',
+		distance: '-100px'
+	})
+
+    //IMAGEM-HOME
+    sr.reveal('.imagem-home-01', {
+		duration: 3000,
+		origin: 'top',
+		distance: '-100px'
+	})
+
+    //MAIS-HOME
+    sr.reveal('.box-titulo-mais-home', {
+		duration: 3000,
+		origin: 'bottom',
+		distance: '-100px'
+	})
+
+    //ITEM-MAIS-HOME-01
+    sr.reveal('.imagem-01', {
+		duration: 4500,
+		origin: 'top',
+		distance: '-100px'
+	})
+
+    //ITEM-MAIS-HOME-02
+    sr.reveal('.imagem-02', {
+		duration: 6000,
+		origin: 'top',
+		distance: '-100px'
+	})
+
+    //ITEM-MAIS-HOME-03
+    sr.reveal('.imagem-03', {
+		duration: 7500,
+		origin: 'top',
+		distance: '-100px'
+	})
+
+    //ITEM-MAIS-HOME-04
+    sr.reveal('.imagem-04', {
+		duration: 9000,
+		origin: 'top',
+		distance: '-100px'
+	})
+
+    //ITEM-MAIS-HOME-05
+    sr.reveal('.imagem-05', {
+		duration: 10500,
+		origin: 'top',
+		distance: '-100px'
+	})
+
+    //img-about
+    sr.reveal('.about-secao-imagem', {
+		duration: 3000,
+		origin: 'right',
+		distance: '-100px'
+	})
+
+    //textos-about
+    sr.reveal('.about__data', {
+		duration: 3000,
+		origin: 'left',
+		distance: '-100px'
+	})
+
+    //titulos-servico
+    sr.reveal('.section__title', {
+		duration: 3000,
+		origin: 'bottom',
+		distance: '-100px'
+	})
+
+     //titulos-servico
+     sr.reveal('.services__data-01', {
+		duration: 3000,
+		origin: 'left',
+		distance: '-100px'
+	})
+
+      //titulos-servico
+      sr.reveal('.services__data-02', {
+		duration: 3000,
+		origin: 'top',
+		distance: '-100px'
+	})
+
+     //titulos-servico
+     sr.reveal('.services__data-03', {
+		duration: 3000,
+		origin: 'right',
+		distance: '-100px'
+	})
+
+     //titulos-servico
+     sr.reveal('.services__data-04', {
+		duration: 3000,
+		origin: 'left',
+		distance: '-100px'
+	})
+
+      //titulos-servico
+      sr.reveal('.services__data-05', {
+		duration: 3000,
+		origin: 'bottom',
+		distance: '-100px'
+	})
+
+     //titulos-servico
+     sr.reveal('.services__data-06', {
+		duration: 3000,
+		origin: 'right',
+		distance: '-100px'
+	})
+
+    //titulos-servico
+    sr.reveal('.section__title-center', {
+		duration: 3000,
+		origin: 'right',
+		distance: '-100px'
+	})
+
+    sr.reveal('.butao-do-app', {
+		duration: 3000,
+		origin: 'left',
+		distance: '-100px'
+	})
+
+    sr.reveal('.product-card', {
+		duration: 3000,
+		origin: 'top',
+		distance: '-100px'
+	})
+
+    sr.reveal('.depoimentos-de-alunos', {
+		duration: 3000,
+		origin: 'bottom',
+		distance: '-100px'
+	})
+
+    sr.reveal('.contact__description', {
+		duration: 3000,
+		origin: 'right',
+		distance: '-100px'
+	})
+
+
+
+    sr.reveal('.contact__content', {
+		duration: 3000,
+		origin: 'left',
+		distance: '-100px'
+	})
+
+    
+    
+
+    
+
+    
+    
+    
